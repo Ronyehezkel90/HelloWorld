7e4d09bd635a51f4c936b624574f2158ce71b176
ronyehezkel <ronyehezkel90@gmail.com>
2022-10-31 21:44:34 +0200

    lesson13

--- app/src/main/java/com/ronyehezkel/helloworld/NotificationsApiInterface.kt
+++ app/src/main/java/com/ronyehezkel/helloworld/NotificationsApiInterface.kt
@@ -7,7 +7,7 @@ import retrofit2.http.*
 
 //(((((call as ExecutorCallbackCall).delegate as OkHttpCall).rawCall as RealCall).originalRequest as Request).headers as Headers).namesAndValues
 interface NotificationsApiInterface {
-    @Headers("Content-Type: application/json", "authorization: key=AAAAk0goG_0:APA91bG1Bpz-rWF7DYn5c_CgWjAQgiD1RKTfC9zBpycSUyboWRlYBi52VOUncDdK0-VoegXq0SMH8LGNF6SZwxj8Sdob-IkcBckifsyLenVzXp5x3T_c7mw0dKXUyrutANgT_cuFPx4I")
+    @Headers("Content-Type: application/json", "authorization: key={token_key}")
     @POST("fcm/send")
     fun sendNotification(@Body requestBody: HashMap<String, Any>): Call<Any>
 
